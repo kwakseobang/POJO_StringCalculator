213d626ac4233c9d232fa6b798020d58cd208fb7
kwakseobang <iii6602@naver.com>
2025-02-13 20:50:24 +0900

    Feat[#3]: ArrayConverter 기능 구현

--- src/main/java/org/kwakmunsu/stringCalculator/ArrayConverter.java
+++ src/main/java/org/kwakmunsu/stringCalculator/ArrayConverter.java
@@ -7,11 +7,17 @@ import java.util.Queue;
 import java.util.stream.Collectors;
 
 public class ArrayConverter {
+
     public static  Queue<Integer> convertStringArrayToIntegerQueue(String[] nums){
+
         List<Integer> list = Arrays.stream(nums)
                 .mapToInt(Integer::parseInt)
                 .boxed().collect(Collectors.toList());
+
         Queue<Integer> queue = new LinkedList<>(list);
+
         return queue;
+
     }
+
 }
