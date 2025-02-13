6479dade8c24b979c660565644f4bfecf561150f
kwakseobang <iii6602@naver.com>
2025-02-13 20:45:28 +0900

    File[#3]: 파일 위치 이동

--- src/main/java/org/kwakmunsu/stringCalculator/error/response/ErrorMessage.java
+++
@@ -1,8 +0,0 @@
-package org.kwakmunsu.stringCalculator.error.response;
-
-public class ErrorMessage {
-    public static final String BAD_REQUEST_OPERAND = "ERROR - 피연산자를 잘못 입력하셨습니다.";
-    public static final String BAD_REQUEST_OPERATOR = "ERROR - 연산자를 잘못 입력하셨습니다.";
-    public static final String ZERO_DIVISION = "ERROR - 0으로 나눌 수 없습니다.";
-    public static final String EXIT = "프로그램을 종료합니다.";
-}
