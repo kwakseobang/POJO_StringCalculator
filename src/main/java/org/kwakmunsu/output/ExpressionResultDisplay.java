bb50b2c8c687b276cd293b2f2d8bd7c5c086b807
kwakseobang <iii6602@naver.com>
2025-02-27 00:52:25 +0900

    Feat: 계산 결과 기능 구현

--- src/main/java/org/kwakmunsu/output/ExpressionResultDisplay.java
+++ src/main/java/org/kwakmunsu/output/ExpressionResultDisplay.java
@@ -1,44 +1,47 @@
-package org.kwakmunsu;
+package org.kwakmunsu.output;
 
 import java.util.stream.IntStream;
-import org.kwakmunsu.OutPut.CalculateResult;
+import org.kwakmunsu.input.Expression;
 
 public class ExpressionResultDisplay {
 
+    private static int printNum = 1;
     private final StringBuilder stringBuilder;
+    private int lastOperandsIndex = 0;
 
     public ExpressionResultDisplay() {
         this.stringBuilder = new StringBuilder();
     }
 
-    public void resultPrinter() {
+    public void resultPrinter(Expression expression, double result) {
+        expressionAppender(expression.operands(), expression.operator());
+        appendResultValue(result);
         System.out.println(stringBuilder);
     }
 
-    public void resultAppender(CalculateResult calculateResult) {
-        stringBuilder.append(calculateResult.printNum()).append(".").append(" ");
-        appendOperandsAndOperator(calculateResult.parsedOperand(), calculateResult.operator());
-        appendResultValue(calculateResult.result());
-    }
+    private void expressionAppender(String[] operands, String operator) {
+        lastOperandsIndex = operands.length - 1;
 
-    private void appendOperandsAndOperator(String[] operands, String operator) {
+        stringBuilder.append(printNum++).append(".").append(" ");
         IntStream.range(0, operands.length).forEach(i -> {
             stringBuilder.append(operands[i]).append(" ");
-            if (i == operands.length - 1) {
-                stringBuilder.append("=").append(" ");
-            } else {
+            if (isNotLastOperand(i)) {
                 stringBuilder.append(operator).append(" ");
+            } else {
+                stringBuilder.append("= ");
             }
         });
     }
 
     private void appendResultValue(double result) {
         if (result % 1 == 0.0) {
-            stringBuilder.append((int) result).append("\n"); // result == n.0 일경우 정수 형변환
+            stringBuilder.append((int) result).append("\n"); // result == n.0 일경우 int 형 변환
         } else {
             stringBuilder.append(result).append("\n");
         }
     }
 
+    private boolean isNotLastOperand(int index) {
+        return index != lastOperandsIndex;
+    }
 }
-
