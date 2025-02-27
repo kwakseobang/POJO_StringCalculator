eb1d6c39591412b6e8bb8fbb562719397190210a
kwakseobang <iii6602@naver.com>
2025-02-27 11:29:58 +0900

    Feat: 예외 메시지 수정

--- src/main/java/org/kwakmunsu/error/ErrorMessage.java
+++ src/main/java/org/kwakmunsu/error/ErrorMessage.java
@@ -5,8 +5,7 @@ public enum ErrorMessage {
     BAD_REQUEST_OPERAND("ERROR - 피연산자를 잘못 입력하셨습니다."),
     BAD_REQUEST_OPERATOR("ERROR - 연산자를 잘못 입력하셨습니다."),
     ZERO_DIVISION("ERROR - 0으로 나눌 수 없습니다."),
-    NOT_MIN_INPUT_EXPRESSION("ERROR - 연산자와 피연산자 모두 입력하셔야 합니다."),
-    NO_SUCH_ELEMENT("ERROR - 입력값이 존재하지 않습니다.");
+    NOT_MIN_INPUT_EXPRESSION("ERROR - 연산자와 피연산자 모두 입력하셔야 합니다.");
 
     private final String message;
 
