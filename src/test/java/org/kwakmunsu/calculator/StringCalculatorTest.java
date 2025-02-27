e0f7c02b694f2e520be1d64d1aa52e4770885130
kwakseobang <iii6602@naver.com>
2025-02-27 10:22:07 +0900

    Test: 0으로 나누기 예외 테스트 추가

--- src/test/java/org/kwakmunsu/calculator/StringCalculatorTest.java
+++ src/test/java/org/kwakmunsu/calculator/StringCalculatorTest.java
@@ -2,9 +2,12 @@ package org.kwakmunsu.calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatCode;
+import static org.assertj.core.api.SoftAssertions.assertSoftly;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.kwakmunsu.error.ErrorMessage;
 import org.kwakmunsu.input.Expression;
 
 class StringCalculatorTest {
@@ -26,6 +29,18 @@ class StringCalculatorTest {
         double result = expressionCalculator.calculateExpression(expression);
         // then
         assertThat(result).isEqualTo(9);
+    }
+
+    @Test
+    void 제로나누기_예외_확인_테스트() {
+        // given
+        String operator = "/";
+        String[] operands = {"3", "0", "4"};
+        Expression expression = new Expression(operands, operator);
+        // when & then
+        assertThatThrownBy(() -> expressionCalculator.calculateExpression(expression))
+            .isInstanceOf(ArithmeticException.class)
+            .hasMessage(ErrorMessage.ZERO_DIVISION.getMessage());
     }
 
 }
